1be9172eda9068ceaf701f4625fd6216d7a99a19
demczenko <demczenko@beliani.co.uk>
2024-02-13 16:03:54 +0100

    server added

--- src/api/apiProvider/init.js
+++ src/api/apiProvider/init.js
@@ -5,7 +5,7 @@ import { ApiDB } from "./ApiDB";
 const APIS = {
   local_storage: () => new ApiLocalStorage(),
   local_json: () => new ApiLocalJson("http://localhost:3000/"),
-  db: () => new ApiDB("http://localhost:7777/"),
+  db: () => new ApiDB("https://server-cms-production.up.railway.app/"),
 };
 
 export const API = APIS.db();
